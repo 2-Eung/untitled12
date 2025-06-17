2528835a83404a3d719055dcc8bcaaef6790133c
2-Eung <eung4256@gmail.com>
2025-06-17 13:53:16 +0900

    throw after

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
 
-
 public class Main {
     public static void main(String[] args) {
-        String input = "10";
-        NumberFormatException e = new NumberFormatException();
-        System.out.println(e);
+        String input = "100";
 
         try {
-            boolean resurt = validateTokenCount(input);
+            validateTokenCount(input);
 
-        if (resurt) {
+
             System.out.println("숫자 2개");
-            }
-
-        } catch (NumberFormatException s) {
+        } catch(NumberFormatException e) {
             System.out.println(e.getMessage());
         }
+
     }
 
-    public static boolean validateTokenCount(String tokens) throws NumberFormatException {
-        if (tokens.length() !=2) {
+    public static void validateTokenCount(String tokens) throws NumberFormatException {
+        if (tokens.length() != 2) {
             throw new NumberFormatException("숫자 2개가 아닙니다.");
         }
-        return true;
     }
 }
